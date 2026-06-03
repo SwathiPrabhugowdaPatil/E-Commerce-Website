922432cd247c2efc5f5e8cb696857c825ef14e0a
Swathi Patil <swathippatil1997@gmail.com>
2026-06-03 10:57:31 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,13 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+const menu = document.querySelector("#menu-icon");
+const navmenu = document.querySelector(".navmenu");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navmenu.classList.toggle("open");
+};
